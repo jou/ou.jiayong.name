76a9c27247b0e017f533013dacba6cc63efc211b
Jiayong Ou <jou@orly.ch>
2010-05-22 18:55:58 +0200

    Categories page

--- _helpers.rb
+++ _helpers.rb
@@ -1,5 +1,19 @@
+require 'cgi'
+
 module Helpers
   def format_date date
     date.strftime('%a, %d %B %Y')
+  end
+
+  def urlencode s
+    CGI.escape s
+  end
+
+  def sort_categories categories
+    categories.to_a.sort
+  end
+
+  def category_url category
+    "/categories.html##{urlencode(category)}"
   end
 end
